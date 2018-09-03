06d2c5fe35a61f89126eb5748061fb99b2a3d06f
DavideMant <mantellini.davide1995@gmail.com>
2018-09-03 16:00:27 +0200

    Update

--- app.js
+++ app.js
@@ -131,7 +131,6 @@ app.post('/creaRepository', function (req, res) {
 
   });
 
-<<<<<<< HEAD
   ConnessioneDB.inserisciDatiRepo(req,res, function(result){
   idRepository = result.idRepository;
   var pathR = "./Server/" + result.idRepository;
@@ -186,63 +185,6 @@ filesaver.folder('Immagini', pathR+"/Immagini", function (err, data) {
   }
 req.session.branch = ConnessioneDB.branchMaster(req, idRepository);
 });
-=======
-  ConnessioneDB.inserisciDatiRepo(req, res, function (result) {
-    idRepository = result.idRepository;
-    var pathR = "./Server/" + result.idRepository;
-    ConnessioneDB.partecipazioneRepo(req, idRepository);
-    var repoDir = pathR + "/.git";
-    fse.ensureDir(path.resolve(__dirname, repoDir)).then(function () {
-      //Inseriamo la repository sul DB
-
-      //Creiamo la cartella .git all'interno della repository
-      return nodegit.Repository.init(path.resolve(__dirname, repoDir), 0);
-    }).then(function (repo) {
-      repository = repo;
-      var fileContent = req.body.readme;
-
-      //aggiungiamo il file README.MD all'interno della working directory
-      return fse.writeFile(path.join(repository.workdir(), fileName), fileContent);
-    }).then(function () {
-      return repository.refreshIndex();
-    })
-      .then(function (idx) {
-        index = idx;
-      })
-      .then(function () {
-        return index.addByPath(fileName);
-      })
-      .then(function () {
-        return index.write();
-      })
-      .then(function () {
-        return index.writeTree();
-      })
-      .then(function (oid) {
-        var dataOdierna = new Date().getTime() / 1000;
-
-        //in author e in committer si scrive: nome, email, data, GMT
-        //abbiamo scritto 120 in quanto è GMT +2 (i minuti in più rispetto al meridiano di Greenwich)
-        var author = nodegit.Signature.create(req.session.nickname, req.session.mail, dataOdierna, 120);
-
-        var committer = nodegit.Signature.create(req.session.nickname, req.session.mail, dataOdierna, 120);
-
-        return repository.createCommit("HEAD", author, committer, "Readme creato", oid, []);
-      }).then(function (commitId) {
-
-      });
-
-
-    //Quando si crea la repository, saranno create le cartelle (vuote inizialmente) IMMAGINI e JSON
-    var filesaver = new Filesaver({ safenames: true });
-
-    filesaver.folder('Immagini', pathR + "/Immagini", function (err, data) {
-      if (err) {
-        console.log("Errore " + err);
-      }
-      req.session.branch = ConnessioneDB.branchMaster(req, res, result);
-    });
->>>>>>> 31720da8e19cf7f087c1291f3374a9a5b7b7398a
 
     filesaver.folder('JSON', pathR + "/JSON", function (err, data) {
       if (err) {
@@ -260,21 +202,12 @@ app.post('/elencoRepo', function (req, res) {
 
 app.post('/settaRepo', function (req, res) {
   req.session.nameRepository = req.body.nomeRepo;
-<<<<<<< HEAD
   ConnessioneDB.settaDatiRepo(req,res, function(result){
     req.session.repository = "./Server/" + result;
     req.session.idRepository = result;
     res.write(res.toString(req.session.repository));
     
     ConnessioneDB.setIdBranchMaster(req,res, function(result){
-=======
-  ConnessioneDB.settaDatiRepo(req, res, function (result) {
-    req.session.repository = "./Server/" + result.idRepository;
-    req.session.idRepository = result.idRepository;
-    res.write(res.toString(req.session.repository));
-
-    ConnessioneDB.setIdBranchMaster(req, res, function (result) {
->>>>>>> 31720da8e19cf7f087c1291f3374a9a5b7b7398a
       req.session.branch = result;
       res.write(res.toString(req.session.branch));
       res.end()
@@ -308,7 +241,6 @@ app.post('/addRevision', function (req, res) {
     } else {
       // console.log('Scritto JPEG');
     }
-<<<<<<< HEAD
     
     var path = req.session.repository;
 
@@ -329,11 +261,6 @@ app.post('/addRevision', function (req, res) {
           req.session.branch = ConnessioneDB.branchMasterRev(req, results);
  
           });
-=======
-
-    ConnessioneDB.settaDatiRepo(req, res, function (result) {
-      ConnessioneDB.insertAddRevision(req.session.repository, req, result.idRepository);
->>>>>>> 31720da8e19cf7f087c1291f3374a9a5b7b7398a
     });
   });
 });
