72ff6b43a74bec7a1892f663ce410dc926c0a6c1
saso93 <salvatore.vestita.1993@gmail.com>
2018-09-03 14:53:15 +0200

    aggiornamento dati db per heroku

--- Backend/database.js
+++ Backend/database.js
@@ -1,9 +1,9 @@
 //File che tiene traccia dei dati relativi alla connessione al database
 module.exports = {
   'connection': {
-      'host': 'localhost',
-      'user': 'root',
-      'password': ''
+      'host': 'eu-cdbr-west-02.cleardb.net',
+      'user': 'ba224649351607',
+      'password': 'c55e3526'
   },
-'database': 'vit',
+'database': 'heroku_9d26d3846af2e4c',
 };
