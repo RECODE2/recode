fcc46b027cafc50f591d412f49592db5769a5d48
Salvatore Vestita <salvatore.vestita.1993@gmail.com>
2018-09-24 21:21:27 +0200

    heroku settings

--- src/js/host.js
+++ src/js/host.js
@@ -1,5 +1,5 @@
 var host = {};
 
-//host.name = "https://recode18.herokuapp.com/";
-host.name = "http://localhost:8081/";
+host.name = "https://recode18.herokuapp.com/";
+//host.name = "http://localhost:8081/";
 export default host;
