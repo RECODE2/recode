98c1d1d8a11cf4da770b2e98295dc735a229d56f
Davide Mantellini <d.mantellini@studenti.uniba.it>
2018-09-25 20:35:23 +0200

    Retry connection to MariaDB until success

--- docker/recode/recode_start.sh
+++ docker/recode/recode_start.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 if [ ! -e /home/recode/db.created ]; then
-	mariadb -h recode_db -u root -e "CREATE DATABASE vit"
-	mariadb -h recode_db -u root vit < /opt/recode/database/recode.sql
+	mariadb -w -h recode_db -u root -e "CREATE DATABASE vit"
+	mariadb -w -h recode_db -u root vit < /opt/recode/database/recode.sql
 	touch /home/recode/db.created
 fi
 
-cd /opt/recode
+if [ -d /home/recode/persistent ]; then
+    cd /home/recode/persistent
+else
+    cd /opt/recode
+fi
+
 npm run start
