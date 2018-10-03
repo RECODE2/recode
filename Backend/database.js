cdf1267bd474b4ce1fd5cedeb774f4136672a943
Tony Mantellini <DavideMant>
2018-10-03 12:41:30 +0200

    Inglese

--- Backend/database.js
+++ Backend/database.js
@@ -9,17 +9,3 @@
 'database': 'vit',
 };
 
-
-// DATI HEROKU
-
-/*
-module.exports = {
-  'connection': {
-      'host': 'eu-cdbr-west-02.cleardb.net',
-      'user': 'ba224649351607',
-      'password': 'c55e3526'
-  },
-'database': 'heroku_9d26d3846af2e4c',
-};
-*/
-
